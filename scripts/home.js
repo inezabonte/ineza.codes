c2a64f6367c795b141c277e802fd249e43bf0536
inezabonte <inezabonte@gmail.com>
2020-09-03 15:06:53 +0200

    changes made to cover image are visible on landing page

--- scripts/home.js
+++ scripts/home.js
@@ -1,17 +1,22 @@
 import firebaseConfig from '../scripts/config.js'
 
-var firebaseConfig = {
-    apiKey: "AIzaSyBjex9XfksKP8_ARn5KPtq2bnL_ZjxuI8o",
-    authDomain: "bonte-mybrand.firebaseapp.com",
-    databaseURL: "https://bonte-mybrand.firebaseio.com",
-    projectId: "bonte-mybrand",
-    storageBucket: "bonte-mybrand.appspot.com",
-    messagingSenderId: "281179086913",
-    appId: "1:281179086913:web:62caf39f9dc4db63efe6c0",
-    measurementId: "G-J10X7VM1ZY"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
-  const db = firebase.firestore()
+const db = firebase.firestore()
+const myImage = document.querySelector(".my-image").style
+
+
+function renderImage (doc){
+   myImage.backgroundImage = `url('${doc.data().coverImage}')`
+}
+
+db.collection("profile").onSnapshot(snapshot => {
+  let changes = snapshot.docChanges()
+  changes.forEach(change => {
+      if(change.type == 'added') {
+         renderImage(change.doc)
+      }
+  })
+})
