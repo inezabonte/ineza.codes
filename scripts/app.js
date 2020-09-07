6e26a970867962e8939a5ca5c1c80faca4117a64
inezabonte <inezabonte@gmail.com>
2020-09-07 12:49:49 +0200

    fixed issue with links for github

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,7 @@
-const auth = firebase.auth()
+const nav = document.querySelector(".nav-links")
 
 // Show dashboard button when user is logged in
-const navLinks = document.querySelector(".nav-links")
+const auth = firebase.auth()
 let dashboard = document.createElement("li")
 let link = document.createElement("a")
 link.setAttribute("href", "../Dashboard/index.html")
@@ -11,13 +11,13 @@ dashboard.appendChild(link)
 auth.onAuthStateChanged(user => {
     if(user){
         document.querySelector(".login").style.display = "none"
-        navLinks.insertAdjacentElement("beforeend", dashboard)
+        nav.insertAdjacentElement("beforeend", dashboard)
     }
 })
 
+//navlinks slide 
 const navSlide =  () => {
     const burger = document.querySelector(".burger")
-    const nav = document.querySelector(".nav-links")
     const navlinks = document.querySelectorAll(".nav-links li")
 
     burger.addEventListener('click', () => {
--- scripts/home.js
+++ scripts/home.js
@@ -20,3 +20,38 @@ db.collection("profile").onSnapshot(snapshot => {
       }
   })
 })
+
+const nav = document.querySelector(".nav-links")
+
+// Show dashboard button when user is logged in
+const auth = firebase.auth()
+let dashboard = document.createElement("li")
+let link = document.createElement("a")
+link.setAttribute("href", "./Dashboard/index.html")
+link.textContent = "Dashboard"
+dashboard.appendChild(link)
+
+auth.onAuthStateChanged(user => {
+    if(user){
+        document.querySelector(".login").style.display = "none"
+        nav.insertAdjacentElement("beforeend", dashboard)
+    }
+})
+
+//navlinks slide 
+const navSlide =  () => {
+    const burger = document.querySelector(".burger")
+    const navlinks = document.querySelectorAll(".nav-links li")
+
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active')
+
+        navlinks.forEach(link => {
+            link.style.animation = `navlink-fade 0.5s ease forwards`
+        })
+    })
+
+    
+}
+
+navSlide()
