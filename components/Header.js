2f1d3c67f08543008f56a2898584da8c3ee40d8c
inezabonte <inezabonte@gmail.com>
2021-04-26 20:18:50 +0200

    added link tag

--- components/Header.js
+++ components/Header.js
@@ -5,6 +5,7 @@ export default function Header() {
 		<Head>
 			<title>Ineza Bonté</title>
 			<meta name="viewport" content="width=device-width, initial-scale=1" />
+			<link rel="icon" href="favicon.ico" />
 		</Head>
 	);
 }
