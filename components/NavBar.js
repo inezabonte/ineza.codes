6fcaa57686aeda786d4f61ff1c7ddf46388daf60
inezabonte <inezabonte@gmail.com>
2021-04-27 16:46:50 +0200

    Fix dark mode

--- components/NavBar.js
+++ components/NavBar.js
@@ -16,11 +16,11 @@ export default function NavBar() {
 	};
 
 	return (
-		<header className="h-16 lg:max-w-6xl px-10 lg:m-auto flex items-center justify-between ">
+		<header className="h-16 lg:max-w-6xl px-10 lg:m-auto flex items-center justify-between">
 			<div>
 				<p className="font-bold text-xl">Ineza Bonté</p>
 			</div>
-			<div className="space-x-4 flex items-center justify-center divide-x dark:divide-gray-700">
+			<div className="space-x-4 flex justify-center ">
 				<div className="space-x-4">
 					<a
 						target="_blank"
@@ -37,21 +37,9 @@ export default function NavBar() {
 						<TwitterOutlined className="text-xl" />
 					</a>
 				</div>
-				<button
-					className="h-auto flex justify-center items-center outline-none focus:outline-none pl-4"
-					onClick={switchTheme}
-				>
-					{theme === "light" ? (
-						<Image
-							src="/Images/moon.svg"
-							width={20}
-							height={20}
-							className="fill-current text-gray-100"
-						/>
-					) : (
-						<Image src="/Images/sun.svg" width={20} height={20} />
-					)}
-				</button>
+				<span className=" text-2xl cursor-pointer" onClick={switchTheme}>
+					{theme === "light" ? "🌑" : "☀️"}
+				</span>
 			</div>
 		</header>
 	);
