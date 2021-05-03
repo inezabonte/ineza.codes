a932708f77d79e47f611da5648f7bfa90cf1a64b
inezabonte <inezabonte@gmail.com>
2021-05-03 17:11:45 +0200

    update icons

--- components/NavBar.js
+++ components/NavBar.js
@@ -4,16 +4,13 @@ import { useTheme } from "next-themes";
 import Link from "next/link";
 
 export default function NavBar() {
-	useEffect(() => {
-		setIsMounted(true);
-	}, []);
-	const [isMounted, setIsMounted] = useState(false);
+	useEffect(() => setMounted(true), []);
+
+	const [mounted, setMounted] = useState(false);
 	const { theme, setTheme } = useTheme();
 
 	const switchTheme = () => {
-		if (isMounted) {
-			setTheme(theme === "light" ? "dark" : "light");
-		}
+		setTheme(theme === "dark" ? "light" : "dark");
 	};
 
 	return (
@@ -45,29 +42,31 @@ export default function NavBar() {
 					className="focus:outline-none outline-none"
 					onClick={switchTheme}
 				>
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						viewBox="0 0 24 24"
-						fill="currentColor"
-						stroke="currentColor"
-						className="h-5 w-5 text-gray-800 dark:text-gray-200"
-					>
-						{theme === "dark" ? (
-							<path
-								strokeLinecap="round"
-								strokeLinejoin="round"
-								strokeWidth={2}
-								d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
-							/>
-						) : (
-							<path
-								strokeLinecap="round"
-								strokeLinejoin="round"
-								strokeWidth={2}
-								d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-							/>
-						)}
-					</svg>
+					{mounted && (
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 24 24"
+							fill="currentColor"
+							stroke="currentColor"
+							className="h-5 w-5 text-gray-800 dark:text-gray-200"
+						>
+							{theme === "dark" ? (
+								<path
+									strokeLinecap="round"
+									strokeLinejoin="round"
+									strokeWidth={2}
+									d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+								/>
+							) : (
+								<path
+									strokeLinecap="round"
+									strokeLinejoin="round"
+									strokeWidth={2}
+									d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+								/>
+							)}
+						</svg>
+					)}
 				</button>
 			</div>
 			<div className="space-x-5 text-2xl col-span-2 justify-self-center lg:col-span-1">
