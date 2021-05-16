0a8607064f1e5bae88e4bf546b652f316846dba9
inezabonte <inezabonte@gmail.com>
2021-05-16 20:48:09 +0200

    feat: adds project section on index page

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,15 @@
 import Image from "next/image";
 import Layout from "../components/Layout";
 import { getFeed } from "../lib/rss";
+import { getGitHubStars } from "../lib/github";
 import { format } from "date-fns";
 import Link from "next/link";
+import { GithubOutlined } from "@ant-design/icons";
 
-export default function index({ feed }) {
+export default function index({ feed, starredRepos }) {
 	return (
 		<Layout page="Ineza Bonté">
-			<main className="space-y-10 px-6 py-10 flex flex-col">
+			<main className="space-y-12 px-6 py-10 flex flex-col">
 				<section className="flex flex-col justify-center items-center lg:flex-row  lg:justify-between ">
 					<div className="mb-10">
 						<Image
@@ -32,8 +34,8 @@ export default function index({ feed }) {
 						</p>
 					</div>
 				</section>
-				<section className="space-y-4 self-center lg:self-start divide-y-2 w-full">
-					<div className="flex justify-between items-center">
+				<section className="space-y-6 self-center lg:self-start w-full">
+					<div className="flex justify-between items-center  border-b-2 pb-4">
 						<h2 className="text-3xl dark:text-white font-bold">
 							Latest Articles
 						</h2>
@@ -44,7 +46,7 @@ export default function index({ feed }) {
 						</Link>
 					</div>
 
-					<div className="space-y-4 pt-4">
+					<div className="space-y-4">
 						{feed.map((article) => (
 							<div className="flex flex-col" key={article.link}>
 								<a
@@ -62,6 +64,49 @@ export default function index({ feed }) {
 						))}
 					</div>
 				</section>
+				<section className="space-y-6 self-center lg:self-start w-full">
+					<div className="flex justify-between items-center border-b-2 pb-4">
+						<h2 className="text-3xl dark:text-white font-bold ">Projects</h2>
+						<a
+							href="https://github.com/inezabonte?tab=repositories"
+							target="_blank"
+							rel="noopener noreferrer"
+							className="text-base bg-gray-200 dark:bg-gray-800 p-2 rounded"
+						>
+							View More
+						</a>
+					</div>
+					<div className="grid grid-cols-1 md:grid-cols-2 justify-items-center gap-4">
+						{starredRepos.map((repo) => (
+							<div
+								key={repo.id}
+								className="p-4 border-2 border-gray-200 dark:border-gray-800 rounded space-y-2 max-w-md"
+							>
+								<div className="flex items-center justify-between">
+									<a
+										href={repo.homepage}
+										target="_blank"
+										rel="noopener noreferrer"
+										className="text-2xl"
+									>
+										{repo.name}
+									</a>
+									<a
+										href={repo.html_url}
+										target="_blank"
+										rel="noopener noreferrer"
+										className="self-start"
+									>
+										<GithubOutlined className="text-2xl" />
+									</a>
+								</div>
+								<p className="text-lg text-gray-500 dark:text-gray-400">
+									{repo.description}
+								</p>
+							</div>
+						))}
+					</div>
+				</section>
 			</main>
 		</Layout>
 	);
@@ -69,10 +114,12 @@ export default function index({ feed }) {
 
 export const getStaticProps = async () => {
 	const feed = await getFeed();
+	const githubStarred = await getGitHubStars();
 
 	return {
 		props: {
 			feed: feed.items.splice(0, 3),
+			starredRepos: githubStarred,
 		},
 		revalidate: 1,
 	};
