60a975768608eb5b1696090d5461e62a9589de77
inezabonte <inezabonte@gmail.com>
2021-05-29 23:44:08 +0200

    date colour

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -20,7 +20,7 @@ export default function Article({ articleData }) {
 			<article className="prose prose-lg dark:prose-dark md:m-auto px-8 md:prose-xl my-4 md:my-16 max-w-2xl">
 				<h2>{articleData.title}</h2>
 				<div className="flex justify-between">
-					<span className="text-gray-600">
+					<span className="text-gray-500">
 						{convertDate(articleData.date, "PPP")}
 					</span>
 					<span className="text-gray-500">{articleData.readTime.text}</span>
