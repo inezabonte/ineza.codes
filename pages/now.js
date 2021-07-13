4c3d911cab0e9d78dac2f204de08b69cd2a43d1d
inezabonte <inezabonte@gmail.com>
2021-07-13 21:40:39 +0200

    update now page

--- pages/now.js
+++ pages/now.js
@@ -7,7 +7,8 @@ export default function now({ frontMatter, mdxSource }) {
 	return (
 		<Layout>
 			<Header title="Now | Ineza Bonté" description={frontMatter.description} />
-			<article className="self-center px-6 prose dark:prose-dark prose-xl  md:my-4 max-w-2xl lg:max-w-3xl">
+			<article className="self-center px-6 prose dark:prose-dark prose-lg  md:my-4 max-w-2xl lg:max-w-3xl">
+				<h1>{frontMatter.title}</h1>
 				<div className="w-full">
 					<MDXRemote {...mdxSource} />
 				</div>
