2f86676b37e10ff5fd5fd51aec743d6296782fc8
inezabonte <inezabonte@gmail.com>
2021-08-09 12:18:23 +0200

    Changed the background

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ class MyDocument extends Document {
 		return (
 			<Html lang="en">
 				<Head />
-				<body className="bg-gray-100 dark:bg-gray-900 dark:text-white">
+				<body>
 					<Main />
 					<NextScript />
 				</body>
