b64bbc0cfc0e8629fe332859174ce966ee9dc819
inezabonte <inezabonte@gmail.com>
2021-10-12 01:41:02 +0200

    fix spam emails

--- components/IntroSection.js
+++ components/IntroSection.js
@@ -11,10 +11,13 @@ export default function IntroSection() {
 					<br />I have a passion for coding and developing Web Applications.
 				</p>
 				<p>
-					You can reach me by email at:{" "}
-					<a href="mailto:inezabonte@gmail.com" className="underline">
-						inezabonte@gmail.com
-					</a>
+					<a
+						href="&#x6d;&#x61;&#x69;&#x6c;&#x74;&#x6f;&colon;&#x69;&#x6e;&#x65;&#x7a;&#x61;&#x62;&#x6f;&#x6e;&#x74;&#x65;&commat;&#x67;&#x6d;&#x61;&#x69;&#x6c;&period;&#x63;&#x6f;&#x6d;"
+						className="underline"
+					>
+						Shoot me an email
+					</a>{" "}
+					if you would like to work together.
 				</p>
 			</div>
 			<div>
