1cc9475692a0a025ae6a3cad52a9858d5e44a50c
inezabonte <inezabonte@gmail.com>
2021-10-12 08:38:38 +0200

    updated link

--- components/IntroSection.js
+++ components/IntroSection.js
@@ -12,7 +12,9 @@ export default function IntroSection() {
 				</p>
 				<p>
 					<a
-						href="&#x6d;&#x61;&#x69;&#x6c;&#x74;&#x6f;&colon;&#x69;&#x6e;&#x65;&#x7a;&#x61;&#x62;&#x6f;&#x6e;&#x74;&#x65;&commat;&#x67;&#x6d;&#x61;&#x69;&#x6c;&period;&#x63;&#x6f;&#x6d;"
+						href={
+							"&#x6d;&#x61;&#x69;&#x6c;&#x74;&#x6f;&colon;&#x69;&#x6e;&#x65;&#x7a;&#x61;&#x62;&#x6f;&#x6e;&#x74;&#x65;&commat;&#x67;&#x6d;&#x61;&#x69;&#x6c;&period;&#x63;&#x6f;&#x6d;"
+						}
 						className="underline"
 					>
 						Shoot me an email
