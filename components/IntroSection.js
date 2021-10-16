4d86ca59d486177bb3f203024f8bbe19c1815281
inezabonte <inezabonte@gmail.com>
2021-10-16 19:16:01 +0200

    email

--- components/IntroSection.js
+++ components/IntroSection.js
@@ -11,12 +11,7 @@ export default function IntroSection() {
 					<br />I have a passion for coding and developing Web Applications.
 				</p>
 				<p>
-					<a
-						href={
-							"&#x6d;&#x61;&#x69;&#x6c;&#x74;&#x6f;&colon;&#x69;&#x6e;&#x65;&#x7a;&#x61;&#x62;&#x6f;&#x6e;&#x74;&#x65;&commat;&#x67;&#x6d;&#x61;&#x69;&#x6c;&period;&#x63;&#x6f;&#x6d;"
-						}
-						className="underline"
-					>
+					<a href="mailto:inezabonte@gmail.com" className="underline">
 						Shoot me an email
 					</a>{" "}
 					if you would like to work together.
