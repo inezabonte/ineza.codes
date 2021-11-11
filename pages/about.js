f602e7098b3e918d07dceca1e2c8dfbe92dc1f46
inezabonte <inezabonte@gmail.com>
2021-11-12 01:53:29 +0200

    update about page

--- pages/about.js
+++ pages/about.js
@@ -1,31 +1,28 @@
 import Layout from "components/Layout";
 import Header from "components/Header";
+import {parseMDXContent} from 'lib/parseMdxContent'
+import { MDXRemote } from "next-mdx-remote";
 
-export default function about() {
+export default function about({mdxSource}) {
 	return (
 		<Layout>
 			<Header title="About | Ineza Bonté" />
 			<main className=" self-center p-6">
-				<h1 className="text-4xl font-bold dark:text-white mb-4">About Me</h1>
-				<div className="prose prose-xl dark:prose-dark max-w-2xl">
-					<p>
-						Hey there, I'm Bonté, welcome to my portfolio. This is my corner of
-						the internet where you get to see what I've been working on and
-						writing about.
-					</p>
-					<p>
-						I got into web development in 2020 just as the pandemic started.
-						With the help of sites like FreeCodeCamp, friends and the Andela
-						Technical Leadership program. I was able to level up my skills to
-						become a FullStack Developer.
-					</p>
-					<p>
-						Apart from coding I enjoy watching movies and listening to music.
-						I'm also trying to learn the guitar, though it's painful in the
-						process of building my calluses.
-					</p>
-				</div>
+			<h1 className="text-4xl font-bold dark:text-white mb-4">About Me</h1>
+				<article className="prose prose-xl dark:prose-dark max-w-2xl">
+					
+					<MDXRemote {...mdxSource}/>
+				</article>
 			</main>
 		</Layout>
 	);
+}
+
+export async function getStaticProps() {
+	const pageData = await parseMDXContent("about", "content/pages")
+	return {
+		props: {
+			...pageData
+		}
+	}
 }
