d525c3239f3eace9d57bfbce34443a5cc7aa0b80
Ineza Bonté Grévy <33348046+inezabonte@users.noreply.github.com>
2023-03-14 18:48:26 -0400

    remove newsletter subscription component

--- pages/index.js
+++ pages/index.js
@@ -7,35 +7,33 @@ import IntroSection from "components/IntroSection";
 import LatestArticles from "components/LatestArticles";
 import ProjectsSection from "components/ProjectsSection";
 import OssContributions from "components/OssContributions";
-import Subscribe from "components/Subscribe";
 
 export default function index({ starredRepos, contributions, articles }) {
-	return (
-		<Layout>
-			<Header title="Ineza Bonté" />
-			<main className="space-y-16 px-6 py-10 flex flex-col self-center">
-				<IntroSection />
-				<LatestArticles articles={articles} />
-				<ProjectsSection starredRepos={starredRepos} />
-				<OssContributions contributions={contributions} />
-				<Subscribe />
-			</main>
-		</Layout>
-	);
+  return (
+    <Layout>
+      <Header title="Ineza Bonté" />
+      <main className="space-y-16 px-6 py-10 flex flex-col self-center">
+        <IntroSection />
+        <LatestArticles articles={articles} />
+        <ProjectsSection starredRepos={starredRepos} />
+        <OssContributions contributions={contributions} />
+      </main>
+    </Layout>
+  );
 }
 
 export const getStaticProps = async () => {
-	const articles = getAllFilesFrontMatter("articles");
-	const githubStarred = await getGitHubStars();
-	const githubContributions = await getGitHubContributions();
-	await generateRssFeed();
+  const articles = getAllFilesFrontMatter("articles");
+  const githubStarred = await getGitHubStars();
+  const githubContributions = await getGitHubContributions();
+  await generateRssFeed();
 
-	return {
-		props: {
-			starredRepos: githubStarred,
-			contributions: githubContributions,
-			articles: articles.splice(0, 3),
-		},
-		revalidate: 1,
-	};
+  return {
+    props: {
+      starredRepos: githubStarred,
+      contributions: githubContributions,
+      articles: articles.splice(0, 3),
+    },
+    revalidate: 1,
+  };
 };
