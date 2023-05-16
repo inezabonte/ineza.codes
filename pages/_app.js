69882ef71c93c6bf1a556ca853a7eb634e00408d
Ineza Bonté Grévy <33348046+inezabonte@users.noreply.github.com>
2023-05-16 02:39:57 -0400

    adding analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import "../styles/global.css";
 
 import { ThemeProvider } from "next-themes";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function MyApp({ Component, pageProps }) {
 	return (
-		<ThemeProvider attribute="class" defaultTheme="system" enableSystem="true">
-			<Component {...pageProps} />
-		</ThemeProvider>
-	);
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem="true">
+      <Component {...pageProps} />
+      <Analytics />
+    </ThemeProvider>
+  );
 }
