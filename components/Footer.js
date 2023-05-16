32256c6e4189f4ddff0550573c5874473097904a
Ineza Bonté Grévy <33348046+inezabonte@users.noreply.github.com>
2023-05-16 02:07:43 -0400

    replace email and update intro

--- components/Footer.js
+++ components/Footer.js
@@ -1,47 +1,51 @@
 import { FaGithub, FaTwitter, FaLinkedin } from "react-icons/fa";
-import Link from "next/link";
+import { FiMail } from "react-icons/fi";
 
 const ExternalLink = ({ children, link, aria }) => (
-	<a
-		target="_blank"
-		rel="noopener noreferrer"
-		className="underline"
-		href={link}
-	>
-		{children}
-	</a>
+  <a
+    target="_blank"
+    rel="noopener noreferrer"
+    className="underline"
+    href={link}
+  >
+    {children}
+  </a>
 );
 
 export default function Footer() {
-	return (
-		<footer className="py-5 mt-auto flex flex-col items-center space-y-4 text-gray-700 dark:text-gray-300 text-sm">
-			<div className="flex flex-col space-y-4 items-center">
-				<div className="space-x-4 flex">
-					<ExternalLink link="https://github.com/inezabonte">
-						<span className="sr-only">GitHub (opens in a new tab)</span>
-						<FaGithub className="text-2xl" />
-					</ExternalLink>
-					<ExternalLink link="https://twitter.com/inezabonte">
-						<span className="sr-only">Twitter (opens in a new tab)</span>
-						<FaTwitter className="text-2xl" />
-					</ExternalLink>
-					<ExternalLink link="https://www.linkedin.com/in/inezabonte/">
-						<span className="sr-only">LinkedIn (opens in a new tab)</span>
-						<FaLinkedin className="text-2xl" />
-					</ExternalLink>
-				</div>
-				<div>
-					<a href="/rss/feed.xml">RSS</a>
-				</div>
-			</div>
-			<div>
-				<p>
-					Made with{" "}
-					<ExternalLink link="https://nextjs.org/">Next.js</ExternalLink>,
-					Deployed on{" "}
-					<ExternalLink link="https://vercel.com/">Vercel</ExternalLink>▲
-				</p>
-			</div>
-		</footer>
-	);
+  return (
+    <footer className="py-5 mt-auto flex flex-col items-center space-y-4 text-gray-700 dark:text-gray-300 text-sm">
+      <div className="flex flex-col space-y-4 items-center">
+        <div className="space-x-4 flex">
+          <ExternalLink link="https://github.com/inezabonte">
+            <span className="sr-only">GitHub (opens in a new tab)</span>
+            <FaGithub className="text-2xl" />
+          </ExternalLink>
+          <ExternalLink link="https://twitter.com/inezabonte">
+            <span className="sr-only">Twitter (opens in a new tab)</span>
+            <FaTwitter className="text-2xl" />
+          </ExternalLink>
+          <ExternalLink link="https://www.linkedin.com/in/inezabonte/">
+            <span className="sr-only">LinkedIn (opens in a new tab)</span>
+            <FaLinkedin className="text-2xl" />
+          </ExternalLink>
+          <a href="mailto:hey@ineza.codes">
+            <span className="sr-only">Mail</span>
+            <FiMail className="text-2xl" />
+          </a>
+        </div>
+        <div>
+          <a href="/rss/feed.xml">RSS</a>
+        </div>
+      </div>
+      <div>
+        <p>
+          Made with{" "}
+          <ExternalLink link="https://nextjs.org/">Next.js</ExternalLink>,
+          Deployed on{" "}
+          <ExternalLink link="https://vercel.com/">Vercel</ExternalLink>▲
+        </p>
+      </div>
+    </footer>
+  );
 }
--- components/IntroSection.js
+++ components/IntroSection.js
@@ -3,28 +3,27 @@ import profilePic from "../public/Images/me.jpg";
 
 export default function IntroSection() {
 	return (
-		<section className="flex flex-col justify-center items-center lg:flex-row-reverse  lg:justify-around space-y-8 ">
-			<div className="lg:self-end max-w-lg space-y-4 prose dark:prose-dark prose-lg md:prose-xl">
-				<h2>I'm Ineza Bonté,</h2>
-				<p>
-					A Fullstack Developer based in Canada 🇨🇦
-					<br />I have a passion for coding and developing Web Applications.
-				</p>
-				<p>
-					<a href="mailto:inezabonte@gmail.com">Shoot me an email</a> if you
-					would like to work together.
-				</p>
-			</div>
-			<div>
-				<Image
-					priority={true}
-					src={profilePic}
-					height={250}
-					width={250}
-					alt="A potrait of Ineza Bonté smiling"
-					className="rounded"
-				/>
-			</div>
-		</section>
-	);
+    <section className="flex flex-col justify-center items-center lg:flex-row-reverse  lg:justify-around space-y-8 ">
+      <div className="lg:self-end max-w-lg space-y-4 prose dark:prose-dark prose-lg md:prose-xl">
+        <h2>I'm Ineza Bonté,</h2>
+        <p>
+          A Fullstack Developer{" "}
+          <a href="https://relevantbits.com">@RelevantBits</a>
+          <br />
+          Currently based in Gatineau, QC 🇨🇦
+          <br />I have a passion for coding and developing Web Applications.
+        </p>
+      </div>
+      <div>
+        <Image
+          priority={true}
+          src={profilePic}
+          height={250}
+          width={250}
+          alt="A potrait of Ineza Bonté smiling"
+          className="rounded"
+        />
+      </div>
+    </section>
+  );
 }
